58b044c7565483c59850cdf464816793db68d3d8
Phil Merrell <philmerrell@boisestate.edu>
2016-09-26 10:26:43 -0600

    Enable prod mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, enableProdMode } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing, appRoutingProviders } from './app.routing';
@@ -15,6 +15,7 @@ import { GoogleMapsService } from './services/google-maps.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TagInputModule } from 'ng2-tag-input';
 
+enableProdMode();
 
 @NgModule({
   declarations: [
