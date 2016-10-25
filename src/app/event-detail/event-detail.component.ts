f2f8ffac149371220e5effa96158efa9bb77b5ad
Phil Merrell <philmerrell@boisestate.edu>
2016-10-25 14:43:07 -0600

    Fix map link FAB

--- src/app/event-detail/event-detail.component.ts
+++ src/app/event-detail/event-detail.component.ts
@@ -19,7 +19,6 @@ export class EventDetailComponent implements OnInit {
         .getCurrentEvent(this.route.snapshot.params['id'])
         .subscribe(event => {
           this.event = event
-          console.log(event);
         });
   }
 
