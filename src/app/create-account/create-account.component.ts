9b3cfaa16065c006ec19eb567ab43e8dfc3bb443
Phil Merrell <philmerrell@boisestate.edu>
2016-10-26 09:18:25 -0600

    style: Improve formatting.

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
-import { emailValidator,lowercaseValidator, uppercaseValidator, numberValidator, symbolValidator, lengthValidator, matchPasswords } from './create-account.validators';
+import { emailValidator, lowercaseValidator, uppercaseValidator, numberValidator, symbolValidator, lengthValidator, matchPasswords } from './create-account.validators';
 import { UserService } from '../services/user.service';
 import { User } from '../services/user.model';
 
@@ -30,15 +30,21 @@ export class CreateAccountComponent implements OnInit {
       'bio'             : ['']
     }, { validator: matchPasswords('password', 'verifyPassword') });
 
-    this.fname = this.accountCreateForm.controls['fname'];
-    this.lname = this.accountCreateForm.controls['lname'];
-    this.email = this.accountCreateForm.controls['email'];
-    this.password = this.accountCreateForm.controls['password'];
+    this.fname          = this.accountCreateForm.controls['fname'];
+    this.lname          = this.accountCreateForm.controls['lname'];
+    this.email          = this.accountCreateForm.controls['email'];
+    this.password       = this.accountCreateForm.controls['password'];
     this.verifyPassword = this.accountCreateForm.controls['verifyPassword'];
-    this.bio = this.accountCreateForm.controls['bio'];
+    this.bio            = this.accountCreateForm.controls['bio'];
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.setInputFocus();
+  }
+
+  setInputFocus() {
+    console.log(this.fname);
+  }
 
   onSubmit() {
     let user = new User(this.fname.value, this.lname.value, this.email.value, this.password.value, this.verifyPassword.value, this.bio.value);
